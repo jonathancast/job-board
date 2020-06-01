6e7b89007285da1bc93e163893118ae9dad559a1
Jonathan Cast <jonathanccast@fastmail.fm>
2020-06-01 17:19:41 -0500

    Add Java tests

---
+++ src/test/java/jonathanccast/jobboard/JobBoardApplicationTests.java
@@ -0,0 +1,11 @@
+package jonathanccast.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
